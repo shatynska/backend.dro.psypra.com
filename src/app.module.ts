f298aaf0bcecb70b2adf49ffb118da41445926c8
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-25 22:39:52 +0300

    Add transactions module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { SpecialistsModule } from './specialists/specialists.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [UsersModule, SpecialistsModule],
+  imports: [UsersModule, SpecialistsModule, TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })

b6957cca69bead0383bf37ea02f2a6d2f18c2db7
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-25 23:03:11 +0300

    Install @nestjs/config and import to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { SpecialistsModule } from './specialists/specialists.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, SpecialistsModule, TransactionsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    SpecialistsModule,
+    TransactionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

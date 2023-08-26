629a1d52c4fba6d3a9c08b3a48ad8a0450f4a29d
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-26 23:44:00 +0300

    Remove some unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { SpecialistsModule } from './specialists/specialists.module';
 import { TransactionsModule } from './transactions/transactions.module';
@@ -13,7 +11,5 @@ import { ConfigModule } from '@nestjs/config';
     SpecialistsModule,
     TransactionsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}

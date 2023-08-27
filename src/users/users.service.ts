db141c1af188459d0ad7f34c0d8355b69c32a13e
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-27 08:24:32 +0300

    Add save, findOne & remove methods in user.service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,30 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '@prisma/prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prismaService: PrismaService) {}
+
+  save(user: Partial<User>) {
+    return this.prismaService.user.create({
+      data: {
+        email: user.email,
+        password: user.password,
+        roles: ['USER'],
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all users`;
+  findOne(idOrEmail: string) {
+    return this.prismaService.user.findFirst({
+      where: {
+        OR: [{ id: idOrEmail }, { email: idOrEmail }],
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    return this.prismaService.user.delete({ where: { id } });
   }
 }

99916bd66e2f990c3ddbbf6df31641d999a7d7f8
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-28 21:50:25 +0300

    Adjust register and login endpoints in auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+} from '@nestjs/common';
+import { LoginDto, RegisterDto } from './dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() dto: RegisterDto) {
+    const user = await this.authService.register(dto);
+    if (!user) {
+      throw new BadRequestException(
+        `Unable to register user with data ${JSON.stringify(dto)}`,
+      );
+    }
+  }
+
+  @Post('login')
+  async login(@Body() dto: LoginDto) {
+    const tokens = await this.authService.login(dto);
+    if (!tokens) {
+      throw new BadRequestException(
+        `Unable to login with data ${JSON.stringify(dto)}`,
+      );
+    }
+    return { accessToken: tokens.accessToken };
+  }
+
+  @Get('refresh')
+  refreshTokens() {}
+}

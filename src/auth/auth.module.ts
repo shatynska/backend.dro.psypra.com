00a147cfdd07ab33e8db9ce04c5348dacb040f1b
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-28 13:49:30 +0300

    Add jwt-module-async-options to config in auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
+import { options } from './config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [UsersModule, PassportModule, JwtModule],
+  imports: [UsersModule, PassportModule, JwtModule.registerAsync(options())],
 })
 export class AuthModule {}

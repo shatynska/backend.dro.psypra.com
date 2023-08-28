36fba26629f6b33cf51e67fe61441b4aa2be6590
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-28 13:24:22 +0300

    Adjust exports in users and auth modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [UsersModule, PassportModule, JwtModule],
 })
 export class AuthModule {}

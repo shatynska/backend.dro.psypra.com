39988393b62ea956701e3ec994464e349bd88f4d
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-28 21:48:45 +0300

    Adjust register and login methods in auth module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { LoginDto, RegisterDto } from './dto';
+import { UsersService } from 'src/users/users.service';
+import { Token, User } from '.prisma/client';
+import { compareSync } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '@prisma/prisma.service';
+import { v4 } from 'uuid';
+import { add } from 'date-fns';
+import { Tokens } from './interfaces';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private readonly logger = new Logger(AuthService.name);
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+    private readonly prismaService: PrismaService,
+  ) {}
+
+  async register(dto: RegisterDto) {
+    const user: User = await this.usersService
+      .findOne(dto.email)
+      .catch((err) => {
+        this.logger.error(err);
+        return null;
+      });
+    if (user) {
+      throw new ConflictException('User with this email is already registered');
+    }
+    return this.usersService.save(dto).catch((err) => {
+      this.logger.error(err);
+      return null;
+    });
+  }
+
+  async login(dto: LoginDto): Promise<Tokens> {
+    const user: User = await this.usersService
+      .findOne(dto.email)
+      .catch((err) => {
+        this.logger.error(err);
+        return null;
+      });
+    if (!user || !compareSync(dto.password, user.password)) {
+      throw new UnauthorizedException('Wrong login or password');
+    }
+
+    const accessToken =
+      'Bearer ' +
+      this.jwtService.sign({
+        id: user.id,
+        email: user.email,
+        roles: user.roles,
+      });
+
+    const refreshToken = await this.getRefreshToken(user.id);
+    return { accessToken, refreshToken };
+  }
+
+  private async getRefreshToken(userId: string): Promise<Token> {
+    return this.prismaService.token.create({
+      data: {
+        token: v4(),
+        exp: add(new Date(), { months: 1 }),
+        userId,
+      },
+    });
+  }
+}

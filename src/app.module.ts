c3c5bc817d83ea1d286617c1c9393151a1830c89
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-29 22:43:57 +0300

    Add JwtAuthGuard as APP_GUARD to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { TransactionsModule } from './transactions/transactions.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from '@auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [
@@ -14,6 +16,12 @@ import { AuthModule } from './auth/auth.module';
     TransactionsModule,
     PrismaModule,
     AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
   ],
 })
 export class AppModule {}

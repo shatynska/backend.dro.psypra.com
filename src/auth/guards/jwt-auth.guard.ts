2f03414d1b38d5d20561b4bbfbb7e245375c598f
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-29 22:59:12 +0300

    Implement  CanActivate interface in JwtAuthGuard

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { isPublic } from '@common/decorators';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') implements CanActivate {
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
+  canActivate(
+    ctx: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const _isPublic = isPublic(ctx, this.reflector);
+    if (_isPublic) {
+      return true;
+    }
+    return super.canActivate(ctx);
+  }
+}

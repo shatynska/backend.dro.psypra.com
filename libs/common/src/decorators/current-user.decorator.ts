226594a595c75ec15b3d17b8e4dcf9bc9d1e7902
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-30 17:55:38 +0300

    Add CurrentUser decorator

---
+++ libs/common/src/decorators/current-user.decorator.ts
@@ -0,0 +1,9 @@
+import { JwtPayload } from '@auth/interfaces';
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+
+export const CurrentUser = createParamDecorator(
+  (key: keyof JwtPayload, ctx: ExecutionContext): JwtPayload => {
+    const request = ctx.switchToHttp().getRequest();
+    return key ? request.user[key] : request.user;
+  },
+);

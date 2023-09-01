c06166b0d2a08d7a17480ee9649effac27658fc7
Helen Shatynska <helen.shatynska@gmail.com>
2023-09-01 13:55:01 +0300

    Add API_PORT to env file and include it to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = await app.get(ConfigService);
+  const port = config.get<number>('API_PORT');
+  await app.listen(port || 3000);
   app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen(3000);
 }
 bootstrap();

23621b5a5c8281971316c087c85f79fd2285ad9d
Helen Shatynska <helen.shatynska@gmail.com>
2023-09-14 22:47:18 +0300

    Init swagger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('DroPsyPra')
+    .setDescription('The DroPsyPra API description')
+    .setVersion('0.0.1')
+    .addTag('users')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/swagger', app, swaggerDocument);
+
   const config = await app.get(ConfigService);
   const port = config.get<number>('API_PORT');
   await app.listen(port || 3000);

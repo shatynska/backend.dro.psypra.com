f6d450ee89e93afd235dd0a670b4064891e4b153
Helen Shatynska <helen.shatynska@gmail.com>
2023-09-21 01:04:07 +0300

    Add validation to UserEntity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,8 +1,20 @@
 import { Provider, Role, User } from '@prisma/client';
 import { Exclude } from 'class-transformer';
+import {
+  ArrayNotEmpty,
+  IsDate,
+  IsEmail,
+  IsNotEmpty,
+  IsUUID,
+} from 'class-validator';
 
 export class UserEntity implements User {
+  @IsUUID(4)
+  @IsNotEmpty()
   id: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @Exclude()
@@ -11,8 +23,15 @@ export class UserEntity implements User {
   @Exclude()
   provider: Provider;
 
+  @IsDate()
+  @IsNotEmpty()
   createdAt: Date;
+
+  @IsDate()
+  @IsNotEmpty()
   updatedAt: Date;
+
+  @ArrayNotEmpty()
   roles: Role[];
 
   constructor(user: User) {

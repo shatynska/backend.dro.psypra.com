d807a519d54909edbc02ec2d3209176a77e6644e
Helen Shatynska <helen.shatynska@gmail.com>
2023-09-23 23:58:22 +0300

    Add @IsEmail comment to prisma schema

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,12 +1,10 @@
 import { PickType } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 import { User } from '../../prisma/entities/user.entity';
 
 export class LoginDto extends PickType(User, ['email', 'password'] as const) {
   email: string;
 
-  @IsString()
-  @MinLength(6)
   @IsNotEmpty()
   password: string;
 }

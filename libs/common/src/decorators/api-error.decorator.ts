c85fe422395f561ab7ec3904e65acf6f10dad015
Helen Shatynska <helen.shatynska@gmail.com>
2023-09-28 21:49:45 +0300

    Add short error message to api error

--- libs/common/src/decorators/api-error.decorator.ts
+++ libs/common/src/decorators/api-error.decorator.ts
@@ -6,6 +6,14 @@ import {
   getSchemaPath,
 } from '@nestjs/swagger';
 
+const errors = {
+  400: 'Bad Request',
+  401: 'Unauthorized',
+  403: 'Forbidden',
+  404: 'Not Found',
+  409: 'Conflict',
+};
+
 export function ApiErrorDecorator(
   statusCode: HttpStatus,
   message?: string,
@@ -20,6 +28,7 @@ export function ApiErrorDecorator(
       schema: {
         default: {
           message: message,
+          error: errors[statusCode] ?? message,
           statusCode: statusCode,
         },
         type: getSchemaPath(ErrorDto),
--- libs/common/src/dto/error.dto.ts
+++ libs/common/src/dto/error.dto.ts
@@ -2,8 +2,11 @@ import { HttpStatus } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ErrorDto {
-  @ApiProperty({ default: 'Internal Server Error' })
+  @ApiProperty({ default: 'The server encountered an unexpected condition' })
   message: string;
+
+  @ApiProperty({ default: 'Internal Server Error' })
+  error: string;
 
   @ApiProperty({ enum: HttpStatus, default: HttpStatus.INTERNAL_SERVER_ERROR })
   statusCode: HttpStatus;

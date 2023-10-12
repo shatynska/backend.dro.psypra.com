4e104d9be4084d6fcbca3f9beb554b224dd0734e
Helen Shatynska <helen.shatynska@gmail.com>
2023-10-12 23:59:03 +0300

    Add more info to MembershipFee entity

--- src/cash-book/domain/membership-fee.entity.ts
+++ src/cash-book/domain/membership-fee.entity.ts
@@ -1,1 +1,10 @@
-export class MembershipFee {}
+import { Member } from './member.value-object';
+import { MembershipFeeId } from './membership-fee-id.value-object';
+import { Month } from './month.value-object';
+
+export class MembershipFee {
+  private readonly id: MembershipFeeId;
+  private amount: number;
+  private month: Month;
+  private member: Member;
+}

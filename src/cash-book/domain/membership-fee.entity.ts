c358d1ee50f8b7a983a3f5d3504a65c0afc24435
Helen Shatynska <helen.shatynska@gmail.com>
2023-10-14 23:57:27 +0300

    Add AmountOfMoney value object

--- src/cash-book/domain/membership-fee.entity.ts
+++ src/cash-book/domain/membership-fee.entity.ts
@@ -1,10 +1,11 @@
+import { AmountOfMoney } from './amount-of-money.value-object';
 import { Member } from './member.value-object';
 import { MembershipFeeId } from './membership-fee-id.value-object';
 import { Month } from './month.value-object';
 
 export class MembershipFee {
   private readonly id: MembershipFeeId;
-  private amount: number;
+  private amount: AmountOfMoney;
   private month: Month;
   private member: Member;
 }

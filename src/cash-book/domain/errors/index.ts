a444f9eec4ba940c34bb5410eae5b5089fc2c0c2
Helen Shatynska <helen.shatynska@gmail.com>
2023-10-16 23:52:42 +0300

    Add constructor and validation in CashBookYear

---
+++ src/cash-book/domain/errors/index.ts
@@ -0,0 +1,1 @@
+export * from './invalid-cash-book-year.error';
--- src/cash-book/domain/value-objects/cash-book-year.value-object.ts
+++ src/cash-book/domain/value-objects/cash-book-year.value-object.ts
@@ -1,1 +1,15 @@
-export class CashBookYear {}
+import { InvalidCashBookYearError } from '../errors';
+
+export class CashBookYear {
+  constructor(private value: number) {
+    this.ensureIsValidCashBookYear(value);
+    this.value = value;
+  }
+
+  private ensureIsValidCashBookYear(value: number) {
+    const currentYear = new Date().getFullYear();
+    if (!Number.isInteger(value) || value < currentYear) {
+      throw new InvalidCashBookYearError();
+    }
+  }
+}

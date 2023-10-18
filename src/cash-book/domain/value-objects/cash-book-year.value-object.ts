dacaaa301c4cbc7572fbf7b2ccbe7ece99f4f0da
Helen Shatynska <helen.shatynska@gmail.com>
2023-10-18 21:42:19 +0300

    Add default value for CashBookYear constructor

--- src/cash-book/domain/value-objects/cash-book-year.value-object.ts
+++ src/cash-book/domain/value-objects/cash-book-year.value-object.ts
@@ -1,12 +1,12 @@
 import { InvalidCashBookYearForCreationError } from '../errors';
 
 export class CashBookYear {
-  constructor(readonly value: number) {
+  constructor(readonly value: number = new Date().getFullYear()) {
     this.ensureIsValidCashBookYearForCreation(value);
     this.value = value;
   }
 
-  private ensureIsValidCashBookYearForCreation(value: number) {
+  private ensureIsValidCashBookYearForCreation(value: number): void {
     const currentYear = new Date().getFullYear();
     const validCashBookYearArray = [
       currentYear - 1,

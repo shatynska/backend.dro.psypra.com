c22ab20b35efdddcca277657198df0ca1cd98789
Helen Shatynska <helen.shatynska@gmail.com>
2023-11-30 16:19:36 +0200

    Add CurrentCashBookStub

--- src/cash-books/cash-books.controller.ts
+++ src/cash-books/cash-books.controller.ts
@@ -8,6 +8,7 @@ import {
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CashBooksService } from './cash-books.service';
 import { CashBalanceResponseDto } from './dto';
+import { CurrentCashBookStub } from './stubs/current-cash-book.stub';
 
 @Controller('cash-books')
 @Public()
@@ -28,125 +29,6 @@ export class CashBooksController {
 
   @Get('current')
   getCurrentCashBooks() {
-    return {
-      reportingMonths: [11, 10, 9, 7, 6, 5],
-      membersWithMembershipFees: [
-        {
-          name: 'Берчук Володимир',
-          membershipFees: [
-            [10, 200],
-            [9, 200],
-            [7, 200],
-            [6, 200],
-          ],
-        },
-        {
-          name: 'Білас Юлія',
-          membershipFees: [
-            [10, 200],
-            [9, 200],
-            [7, 200],
-          ],
-        },
-        {
-          name: 'Гамбарова Еляна',
-          membershipFees: [
-            [10, 200],
-            [9, 200],
-            [7, 200],
-          ],
-        },
-        {
-          name: 'Дробчак Ольга',
-          membershipFees: [
-            [10, 200],
-            [9, 200],
-            [7, 200],
-            [6, 200],
-            [5, 200],
-          ],
-        },
-        {
-          name: 'Коцюба Галина',
-          membershipFees: [
-            [6, 200],
-            [5, 500],
-          ],
-        },
-        {
-          name: 'Лех Наталія',
-          membershipFees: [
-            [10, 200],
-            [9, 200],
-            [7, 200],
-            [6, 200],
-            [5, 200],
-          ],
-        },
-        {
-          name: 'Некрасова Валентина',
-          membershipFees: [
-            [10, 200],
-            [9, 200],
-            [7, 200],
-            [6, 200],
-            [5, 200],
-          ],
-        },
-        {
-          name: 'Петришин Ольга',
-          membershipFees: [
-            [10, 200],
-            [9, 200],
-            [7, 200],
-            [6, 200],
-            [5, 200],
-          ],
-        },
-        {
-          name: 'Созанська Ірина',
-          membershipFees: [
-            [10, 200],
-            [9, 200],
-            [7, 200],
-            [6, 200],
-            [5, 200],
-          ],
-        },
-        {
-          name: 'Татаревич Галина',
-          membershipFees: [[10, 200]],
-        },
-        {
-          name: 'Франчук Тетяна',
-          membershipFees: [[7, 200]],
-        },
-        {
-          name: 'Чапля Уляна',
-          membershipFees: [
-            [10, 200],
-            [9, 200],
-            [7, 200],
-            [6, 200],
-            [5, 200],
-          ],
-        },
-        {
-          name: 'Шатинська Альона',
-          membershipFees: [
-            [10, 200],
-            [9, 200],
-            [7, 200],
-            [6, 200],
-            [5, 200],
-          ],
-        },
-      ],
-      expenses: [
-        { id: '1', date: '2029', amount: 45000, details: 'Піца' },
-        { id: '2', date: '2029', amount: 19000, details: 'Алкоголь' },
-        { id: '3', date: '2025', amount: 45000, details: 'Піца' },
-      ],
-    };
+    return CurrentCashBookStub;
   }
 }

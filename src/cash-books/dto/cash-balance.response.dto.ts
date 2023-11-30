e81f0d5010dfcf172107634fc2ec156b89830c06
Helen Shatynska <helen.shatynska@gmail.com>
2023-11-30 13:07:43 +0200

    Update CashBalanceResponseDto

--- src/cash-books/dto/cash-balance.response.dto.ts
+++ src/cash-books/dto/cash-balance.response.dto.ts
@@ -1,10 +1,6 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { CashBalance } from '@prisma/entities/cash-balance.entity';
+import { CashBalance } from '@prisma/entities/cashBalance.entity';
 
 export class CashBalanceResponseDto extends CashBalance {
-  @ApiProperty({ example: '5600' })
-  value: number;
-
   constructor(cashBalance: CashBalance) {
     super();
     Object.assign(this, cashBalance);

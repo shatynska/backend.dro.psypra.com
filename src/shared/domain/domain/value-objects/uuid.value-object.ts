bcdbf04f18c1510faba5c0a4478390e4208b6c20
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-01 16:05:16 +0200

    Add Entity, ValueObject & AggregateRoot abstract classes to shared/domain folder

--- src/shared/domain/domain/value-objects/uuid.value-object.ts
+++ src/shared/domain/domain/value-objects/uuid.value-object.ts
@@ -1,9 +1,11 @@
 import { v4 as uuid } from 'uuid';
+import { ValueObject } from './value-object';
 
-export class Uuid {
+export class Uuid extends ValueObject {
   readonly value: string;
 
   constructor() {
+    super();
     this.value = uuid();
   }
 }

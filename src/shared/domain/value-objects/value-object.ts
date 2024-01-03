98cb71c363d047b8d1e11fd70ab5166596e41c49
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-03 23:59:14 +0200

    Add constructor & getValue method to  ValueObject

--- src/shared/domain/value-objects/value-object.ts
+++ src/shared/domain/value-objects/value-object.ts
@@ -1,3 +1,11 @@
-export abstract class ValueObject {
-  // TODO
+export abstract class ValueObject<T> {
+  readonly props: T;
+
+  constructor(props: T) {
+    this.props = Object.freeze(props);
+  }
+
+  public getValue() {
+    return this.props;
+  }
 }

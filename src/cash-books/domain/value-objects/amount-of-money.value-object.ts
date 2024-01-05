40821ee16808c5feefb5e54e8f910eb6927d2a65
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-05 23:59:44 +0200

    Update AmountOfMoney & CashBookTitle

--- src/cash-books/domain/value-objects/amount-of-money.value-object.ts
+++ src/cash-books/domain/value-objects/amount-of-money.value-object.ts
@@ -1,5 +1,15 @@
+import { failure } from '~/shared/core/result';
 import { ValueObject } from '~/shared/domain/value-objects';
 
-export class AmountOfMoney extends ValueObject {
-  // TODO
+export class AmountOfMoney extends ValueObject<number> {
+  constructor(value: number = 0) {
+    super(value);
+    this.validate();
+  }
+
+  private validate() {
+    if (this.value < 0) {
+      return failure(new Error());
+    }
+  }
 }
--- src/cash-books/domain/value-objects/cash-book-title.value-object.ts
+++ src/cash-books/domain/value-objects/cash-book-title.value-object.ts
@@ -1,8 +1,16 @@
+import { failure } from '~/shared/core/result';
 import { ValueObject } from '~/shared/domain/value-objects';
 
-export class CashBookTitle extends ValueObject {
-  constructor(private readonly title: string) {
-    super();
+export class CashBookTitle extends ValueObject<string> {
+  constructor(value: string) {
+    super(value);
+    this.validate();
   }
-  // TODO
+
+  private validate() {
+    const length = this.value.length;
+    if (length < 2 && length > 50) {
+      return failure(new Error());
+    }
+  }
 }

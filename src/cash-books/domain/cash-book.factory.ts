7c6c18339a8b8e10355e704e301846216f322af2
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-08 00:17:56 +0200

    Refine create() & add reconstitute() in CashBookFactory

--- src/cash-books/domain/cash-book.factory.ts
+++ src/cash-books/domain/cash-book.factory.ts
@@ -1,9 +1,31 @@
+import { Result, failure, success } from '~/shared/core/result';
 import { Uuid } from '~/shared/domain/value-objects';
 import { CashBook } from './cash-book.aggregate-root';
-import { CashBookTitle } from './value-objects';
+import { CashBookDto } from './dto/cash-book.dto';
+import { AmountOfMoney, CashBookTitle } from './value-objects';
 
 export class CashBookFactory {
-  public static async create(title: string) {
-    return new CashBook(new Uuid(), new CashBookTitle(title));
+  static create(props: CashBookDto): Result<Error, CashBook> {
+    const cashBook = new CashBook(new Uuid(), new CashBookTitle(props.title));
+
+    if (!cashBook) {
+      return failure(new Error());
+    }
+
+    return success(cashBook);
+  }
+
+  static reconstitute(props: Required<CashBookDto>): Result<Error, CashBook> {
+    const cashBook = new CashBook(
+      new Uuid(props.id),
+      new CashBookTitle(props.title),
+      new AmountOfMoney(props.cashBalance),
+    );
+
+    if (!cashBook) {
+      return failure(new Error());
+    }
+
+    return success(cashBook);
   }
 }

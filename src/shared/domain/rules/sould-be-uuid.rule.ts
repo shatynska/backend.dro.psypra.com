c7a29c617c7c0be847b59432c3b2f5959d14bcaf
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-12 23:47:13 +0200

    Add ShouldBeUuidV4Rule

---
+++ src/shared/domain/rules/sould-be-uuid.rule.ts
@@ -0,0 +1,15 @@
+import { DomainError } from '../errors/domain.error';
+import { Rule } from './rule';
+
+export class ShouldBeUuidV4Rule implements Rule {
+  constructor(private value: string) {}
+
+  // TODO Replace message with constant
+  public error = new DomainError('Значення повинно бути в форматі UUID v4');
+
+  public isBroken(): boolean {
+    const regex =
+      /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
+    return regex.test(this.value) === false;
+  }
+}

4541791208421ac45bef7b5c803c872e127df7ea
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-12 23:44:18 +0200

    Add validate method to ValueObject

--- src/shared/domain/value-objects/value-object.ts
+++ src/shared/domain/value-objects/value-object.ts
@@ -1,6 +1,26 @@
+import { Result, failure, success } from '~/shared/core/result';
+import { DomainErrors } from '../errors/domain.errors';
+import { Rule } from '../rules/rule';
+
 export abstract class ValueObject<T> {
-  constructor(protected readonly value: T) {
+  protected constructor(protected readonly value: T) {
     this.value = Object.freeze(value);
+  }
+
+  protected static validate(rules: Rule[]): Result<DomainErrors, true> {
+    const domainErrors: DomainErrors = new DomainErrors([]);
+
+    for (const rule of rules) {
+      if (rule.isBroken()) {
+        domainErrors.errors.push(rule.error);
+      }
+    }
+
+    if (domainErrors.errors.length > 0) {
+      return failure(domainErrors);
+    }
+
+    return success(true);
   }
 
   public getValue() {

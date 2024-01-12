351012ca9b6415976ef94f205651b664da0fe9f3
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-12 23:46:26 +0200

    Add ShouldBeWithinLengthRangeRule

---
+++ src/shared/domain/rules/should-be-within-length-range.rule.ts
@@ -0,0 +1,20 @@
+import { DomainError } from '../errors/domain.error';
+import { Rule } from './rule';
+
+export class ShouldBeWithinLengthRangeRule implements Rule {
+  constructor(
+    private value: string,
+    private minLength: number,
+    private maxLength: number,
+  ) {}
+
+  // TODO Replace message with constant
+  public error = new DomainError(
+    `Значення повинно бути довжиною від ${this.minLength} до ${this.maxLength} символів`,
+  );
+
+  public isBroken(): boolean {
+    const length = this.value.length;
+    return (length >= this.minLength && length <= this.maxLength) === false;
+  }
+}

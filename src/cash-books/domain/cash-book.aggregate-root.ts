96b6bf6e1a976f9550b186356fbe95ac19f8e5c5
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-15 16:05:45 +0200

    Declare CashBook constructor as private & move create & reconstitute method from factory

--- src/cash-books/domain/cash-book.aggregate-root.ts
+++ src/cash-books/domain/cash-book.aggregate-root.ts
@@ -1,14 +1,40 @@
+import { Result, failure, success } from '~/shared/core/result';
 import { AggregateRoot } from '~/shared/domain/aggregate-root';
 import { Uuid } from '~/shared/domain/value-objects';
-import { AmountOfMoney, CashBookTitle } from './value-objects';
+import { AmountOfMoney } from '~/shared/domain/value-objects/amount-of-money.value-object';
+import { Title } from '~/shared/domain/value-objects/title.value-object';
 
 export class CashBook extends AggregateRoot {
-  constructor(
+  private constructor(
     private id: Uuid = new Uuid(),
-    private title: CashBookTitle,
+    private title: Title,
     private cashBalance: AmountOfMoney = new AmountOfMoney(),
   ) {
     super();
+  }
+
+  static create(props: CashBook): Result<Error, CashBook> {
+    const cashBook = new CashBook(new Uuid(), new CashBookTitle(props.title));
+
+    if (!cashBook) {
+      return failure(new Error());
+    }
+
+    return success(cashBook);
+  }
+
+  static reconstitute(props: Required<CashBook>): Result<Error, CashBook> {
+    const cashBook = new CashBook(
+      new Uuid(props.id),
+      new Title(props.title),
+      new AmountOfMoney(props.cashBalance),
+    );
+
+    if (!cashBook) {
+      return failure(new Error());
+    }
+
+    return success(cashBook);
   }
 
   public mapToPrimitives() {

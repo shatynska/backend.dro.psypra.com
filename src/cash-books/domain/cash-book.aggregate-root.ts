3864c7ceba38336e55e75e881b455227f78226af
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-15 22:49:29 +0200

    Add DomainErrors to handle validate errors in value objects in cashBook create method

--- src/cash-books/domain/cash-book.aggregate-root.ts
+++ src/cash-books/domain/cash-book.aggregate-root.ts
@@ -1,5 +1,6 @@
 import { Result, failure, success } from '~/shared/core/result';
 import { AggregateRoot } from '~/shared/domain/aggregate-root';
+import { DomainError, DomainErrors } from '~/shared/domain/errors';
 import { AmountOfMoney, Title, Uuid } from '~/shared/domain/value-objects';
 
 export type CashBookParameters = {
@@ -12,18 +13,41 @@ export type CreateCashBookParameters = Pick<CashBookParameters, 'title'>;
 
 export class CashBook extends AggregateRoot {
   private constructor(
-    private id: Uuid = new Uuid(),
+    private readonly id: Uuid,
     private title: Title,
-    private cashBalance: AmountOfMoney = new AmountOfMoney(),
+    private cashBalance: AmountOfMoney,
   ) {
     super();
   }
 
-  static create(params: CreateCashBookParameters): Result<Error, CashBook> {
-    const cashBook = new CashBook(new Uuid(), new CashBookTitle(params.title));
+  static create(
+    params: CreateCashBookParameters,
+  ): Result<DomainErrors, CashBook> {
+    const domainErrors: DomainErrors = new DomainErrors([]);
 
+    const title = Title.create(params.title);
+    if (title.isFailure()) {
+      domainErrors.errors.push(title.value);
+    }
+
+    const id = Uuid.create();
+    const cashBalance = AmountOfMoney.create(0);
+
+    if (domainErrors.errors.length > 0) {
+      return failure(domainErrors);
+    }
+
+    const cashBook = new CashBook(
+      id.value as Uuid,
+      title.value as Title,
+      cashBalance.value as AmountOfMoney,
+    );
     if (!cashBook) {
-      return failure(new Error());
+      domainErrors.errors.push(
+        // TODO Replace message with constant
+        new DomainError('Помилка при створенні касової книги'),
+      );
+      return failure(domainErrors);
     }
 
     return success(cashBook);

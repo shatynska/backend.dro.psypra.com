fbd95493f7217df53eaa6254e237ba07db5fe18e
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-16 23:08:40 +0200

    Remove Result from CashBooksWriteRepository & update parameters & return types

--- src/cash-books/domain/cash-books.write.repository.ts
+++ src/cash-books/domain/cash-books.write.repository.ts
@@ -1,13 +1,11 @@
-import { Result } from '~/shared/core/result';
 import { CashBook } from './cash-book.aggregate-root';
-import { CashBookDto } from './dto/cash-book.dto';
 
 export const CASH_BOOKS_WRITE_REPOSITORY_TOKEN = Symbol(
   'CashBooksWriteRepositoryToken',
 );
 
 export interface CashBooksWriteRepository {
-  save(props: CashBookDto): Promise<Result<Error, void>>;
+  save(cashBook: CashBook): Promise<void>;
 
-  getById(id: string): Promise<Result<Error, CashBook>>;
+  getById(id: string): Promise<CashBook | null>;
 }

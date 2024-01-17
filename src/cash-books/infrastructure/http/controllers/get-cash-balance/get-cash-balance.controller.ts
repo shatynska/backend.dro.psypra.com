51e491165c20b18daee59280f510302ff9aecd41
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-17 22:33:37 +0200

    Replace BadRequestException with NotFoundException in GetCashBalanceController

--- src/cash-books/infrastructure/http/controllers/get-cash-balance/get-cash-balance.controller.ts
+++ src/cash-books/infrastructure/http/controllers/get-cash-balance/get-cash-balance.controller.ts
@@ -1,14 +1,17 @@
 import { ApiErrorDecorator, Public } from '@common/decorators';
 import {
-  BadRequestException,
   Controller,
   Get,
   HttpStatus,
+  NotFoundException,
   Param,
 } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CashBalanceDto } from '~/cash-books/application/dto/cash-balance.dto';
+import { CashBookNotFoundError } from '~/cash-books/application/errors/cash-book-not-found.error';
 import { GetCashBalanceQuery } from '~/cash-books/application/queries/get-cash-balance/get-cash-balance.query';
+import { Result } from '~/shared/core/result';
 import { CashBalanceResponseDto } from '../../dto/responses/cash-balance.response.dto';
 
 @Controller('cash-books')
@@ -21,17 +24,16 @@ export class GetCashBalanceController {
     status: 200,
     type: CashBalanceResponseDto,
   })
-  @ApiErrorDecorator(HttpStatus.BAD_REQUEST, 'Bad Request')
+  @ApiErrorDecorator(HttpStatus.NOT_FOUND, CashBookNotFoundError.message)
   @Get('cash-balance/:id')
   async getCashBalance(
     @Param('id') id: string,
   ): Promise<CashBalanceResponseDto> {
-    const cashBalance = await this.queryBus.execute(
-      new GetCashBalanceQuery(id),
-    );
+    const cashBalance: Result<CashBookNotFoundError, CashBalanceDto> =
+      await this.queryBus.execute(new GetCashBalanceQuery(id));
 
     if (cashBalance.isFailure()) {
-      throw new BadRequestException(cashBalance.value.message);
+      throw new NotFoundException(cashBalance.value.message);
     }
 
     return new CashBalanceResponseDto(cashBalance.value);

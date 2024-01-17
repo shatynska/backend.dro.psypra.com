5b609d92a757afa7cca98fe6cf90444c6937d108
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-17 18:33:52 +0200

    Change pathname of CashBook unit test & update it

---
+++ src/cash-books/domain/cash-book.spec.ts
@@ -0,0 +1,20 @@
+import { CashBook } from './cash-book.aggregate-root';
+
+describe('CashBook', () => {
+  jest.mock('~/shared/domain/value-objects');
+
+  it('should be defined', () => {
+    expect(CashBook).toBeDefined();
+  });
+
+  it('should create cash book with defined title', () => {
+    const received = CashBook.create({ title: 'test' });
+    const newCashBook = received.value as CashBook;
+
+    expect(received.value).toBeInstanceOf(CashBook);
+
+    expect(newCashBook.getTitle()).toEqual('test');
+  });
+});
+
+// TODO Test reconstitute method & others

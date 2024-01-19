8230d89c08e3aadfe337f7045493b846b023e680
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-19 22:36:53 +0200

    Remove Result, DomainErrors, & true return type in ValueObject class

--- src/shared/domain/value-objects/value-object.ts
+++ src/shared/domain/value-objects/value-object.ts
@@ -1,5 +1,4 @@
-import { Result, failure, success } from '~/shared/core/result';
-import { DomainErrors } from '../errors/domain.errors';
+import { DomainError } from '../errors';
 import { Rule } from '../rules/rule';
 
 export abstract class ValueObject<T> {
@@ -7,20 +6,16 @@ export abstract class ValueObject<T> {
     this.value = Object.freeze(value);
   }
 
-  protected static validate(rules: Rule[]): Result<DomainErrors, true> {
-    const domainErrors: DomainErrors = new DomainErrors();
+  protected static validate(rules: Rule[]): DomainError[] {
+    const errors: DomainError[] = [];
 
     for (const rule of rules) {
       if (rule.isBroken()) {
-        domainErrors.errors.push(rule.error);
+        errors.push(rule.error);
       }
     }
 
-    if (domainErrors.errors.length > 0) {
-      return failure(domainErrors);
-    }
-
-    return success(true);
+    return errors;
   }
 
   public getValue() {

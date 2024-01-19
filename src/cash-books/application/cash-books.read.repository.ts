0584fcdf62ba29d7a56f25d605d247ba56d76205
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-20 00:51:44 +0200

    Remove Result from CashBooksReadRepository return type & set Promise<null> as possible return result

--- src/cash-books/application/cash-books.read.repository.ts
+++ src/cash-books/application/cash-books.read.repository.ts
@@ -1,10 +1,9 @@
 import { CashBookDto } from '~/cash-books/application/dto/cash-book.dto';
-import { Result } from '~/shared/core/result';
 
 export const CASH_BOOKS_READ_REPOSITORY_TOKEN = Symbol(
   'CashBooksReadRepositoryToken',
 );
 
 export interface CashBooksReadRepository {
-  getById(id: string): Promise<Result<Error, CashBookDto>>;
+  getById(id: string): Promise<CashBookDto | null>;
 }

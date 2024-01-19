3e117bbc4fbdee7824a053ca4c2947a4108b6c24
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-19 22:34:05 +0200

    Add public getJoinedMessages and private getMessages methods in DomainErrors class

--- src/shared/domain/errors/domain.errors.ts
+++ src/shared/domain/errors/domain.errors.ts
@@ -4,4 +4,26 @@ export class DomainErrors extends AggregateError {
   constructor(errors: DomainError[] = [], message?: string) {
     super(errors, message);
   }
+
+  public getJoinedMessages(): string {
+    return this.getMessages(this).join('. ');
+  }
+
+  private getMessages(domainErrors: DomainErrors): string[] {
+    const stack: DomainErrors[] = [domainErrors];
+    const messages: string[] = [];
+
+    while (stack.length > 0) {
+      const error = stack.pop();
+      if (error) {
+        messages.push(error.message);
+
+        if (error.errors) {
+          stack.push(...error.errors);
+        }
+      }
+    }
+
+    return messages;
+  }
 }

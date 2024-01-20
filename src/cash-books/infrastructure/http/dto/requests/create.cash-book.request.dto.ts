e37e219173221ccd4dbe485d01938a8f4e90e89d
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-20 22:03:27 +0200

    Add @IsString to title property of CreateCashBookRequestDto

--- src/cash-books/infrastructure/http/dto/requests/create.cash-book.request.dto.ts
+++ src/cash-books/infrastructure/http/dto/requests/create.cash-book.request.dto.ts
@@ -1,6 +1,8 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { CreateCashBookDto } from '~/cash-books/application/dto/create-cash-book.dto';
 
-export class CreateCashBookRequestDto {
+export class CreateCashBookRequestDto implements CreateCashBookDto {
   @IsNotEmpty()
+  @IsString()
   title: string;
 }

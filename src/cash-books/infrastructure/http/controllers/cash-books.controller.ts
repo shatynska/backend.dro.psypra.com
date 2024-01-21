b09f65fae3765c0818a8904fa63e69e9c76f2bfa
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-21 23:12:21 +0200

    Remove CashBooksService & update related files

--- src/cash-books/infrastructure/http/controllers/cash-books.controller.ts
+++ src/cash-books/infrastructure/http/controllers/cash-books.controller.ts
@@ -1,15 +1,12 @@
 import { Public } from '@common/decorators';
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { CashBooksService } from '../../../application/cash-books.service';
 import { CurrentCashBookResponseDtoStub } from '../dto/responses/current-cash-book.response.dto.stub';
 
 @Controller('cash-books')
 @Public()
 @ApiTags('cash-books')
 export class CashBooksController {
-  constructor(private readonly cashBooksService: CashBooksService) {}
-
   @Get('current')
   getCurrentCashBooks() {
     return CurrentCashBookResponseDtoStub;
--- src/cash-books/infrastructure/http/http.module.ts
+++ src/cash-books/infrastructure/http/http.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { CreateCashBookHandler } from '~/cash-books/application/commands/create-cash-book/create-cash-book.handle';
 import { GetCashBalanceHandler } from '~/cash-books/application/queries/get-cash-balance/get-cash-balance.handle';
-import { CashBooksService } from '../../application/cash-books.service';
 import { PersistenceModule } from '../persistence/persistence.module';
 import { CashBooksController } from './controllers/cash-books.controller';
 import { CreateCashBookController } from './controllers/create-cash-book/create-cash-book.controller';
@@ -15,6 +14,6 @@ import { GetCashBalanceController } from './controllers/get-cash-balance/get-cas
     CreateCashBookController,
     CashBooksController,
   ],
-  providers: [CashBooksService, GetCashBalanceHandler, CreateCashBookHandler],
+  providers: [GetCashBalanceHandler, CreateCashBookHandler],
 })
 export class HttpModule {}

d9b6c9e5dd361773fa98939abad279ccb18e9e4f
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-21 22:58:59 +0200

    Move MembershipFee & Member to separate module

--- src/cash-books/domain/entities/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './expense.entity';
-export * from './member.entity';
-export * from './membership-fee.entity';
-export * from './reporting-month.entity';
--- src/membership-fees/domain/membership-fee.entity.ts
+++ src/membership-fees/domain/membership-fee.entity.ts
@@ -1,12 +1,10 @@
 import { Entity } from '~/shared/domain/entities';
-import { Uuid } from '~/shared/domain/value-objects';
-import { AmountOfMoney } from '../value-objects';
+import { AmountOfMoney, Uuid } from '~/shared/domain/value-objects';
 import { Member } from './member.entity';
-import { ReportingMonth } from './reporting-month.entity';
 
 export class MembershipFee extends Entity {
   private readonly id: Uuid;
   private amount: AmountOfMoney;
-  private reportingMonth: ReportingMonth;
+  private reportingMonthId: Uuid;
   private member: Member;
 }

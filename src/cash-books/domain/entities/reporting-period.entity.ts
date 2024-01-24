575a5f530681b5b644ab38648ea97b40300c1061
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-24 23:33:01 +0200

    Add create method to ReportingPeriod entity

--- src/cash-books/domain/entities/reporting-period.entity.ts
+++ src/cash-books/domain/entities/reporting-period.entity.ts
@@ -1,11 +1,18 @@
+import { Result, failure, success } from '~/shared/core/result';
 import { Entity } from '~/shared/domain/entities';
 import { Title } from '~/shared/domain/value-objects';
 import { Uuid } from '~/shared/domain/value-objects/uuid.value-object';
+import { ReportingPeriodCreationError } from '../errors';
 
 export type ReportingPeriodPrimitives = {
   id: string;
   title: string;
 };
+
+export type CreateReportingPeriodParameters = Pick<
+  ReportingPeriodPrimitives,
+  'title'
+> & { isTitleUnique: boolean };
 
 export class ReportingPeriod extends Entity {
   private constructor(
@@ -13,6 +20,36 @@ export class ReportingPeriod extends Entity {
     private title: Title,
   ) {
     super();
+  }
+
+  static create(
+    params: CreateReportingPeriodParameters,
+  ): Result<ReportingPeriodCreationError, ReportingPeriod> {
+    const { title: titleValue, isTitleUnique } = params;
+
+    const creatingError = new ReportingPeriodCreationError();
+
+    const title = Title.create({
+      value: titleValue,
+      isUnique: isTitleUnique,
+    });
+
+    if (title.isFailure()) {
+      creatingError.errors.push(title.value);
+    }
+
+    const id = Uuid.create();
+
+    if (creatingError.errors.length > 0) {
+      return failure(creatingError);
+    }
+
+    const reportingPeriod = new ReportingPeriod(
+      id.value as Uuid,
+      title.value as Title,
+    );
+
+    return success(reportingPeriod);
   }
 
   mapToPrimitives(): ReportingPeriodPrimitives {

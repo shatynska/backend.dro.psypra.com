befa42f1f74d078bde31b606b658e823aec3bbd4
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-25 12:26:08 +0200

    Fix pathname of CreateCashBookRequestDto & refactor CreateCashBookController

--- src/cash-books/infrastructure/http/controllers/create-cash-book/create-cash-book.controller.ts
+++ src/cash-books/infrastructure/http/controllers/create-cash-book/create-cash-book.controller.ts
@@ -11,29 +11,27 @@ import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateCashBookCommand } from '~/cash-books/application/commands/create-cash-book/create-cash-book.command';
 import { CashBookCreationError } from '~/cash-books/domain/errors';
 import { Result } from '~/shared/core/result';
-import { CreateCashBookRequestDto } from '../../dto/requests/create.cash-book.request.dto';
+import { CreateCashBookRequestDto } from '../../dto/requests/create-cash-book.request.dto';
 
 @Controller('cash-books')
 @Public()
 @ApiTags('cash-books')
 export class CreateCashBookController {
   constructor(private readonly commandBus: CommandBus) {}
+
   @ApiResponse({ status: 201 })
   @ApiErrorDecorator(
     HttpStatus.BAD_REQUEST,
     CashBookCreationError.defaultMessage,
   )
   @Post()
-  async createCashBook(
-    @Body() createCashBookRequestDto: CreateCashBookRequestDto,
-  ): Promise<void> {
-    const cashBook: Result<CashBookCreationError, void> =
-      await this.commandBus.execute(
-        new CreateCashBookCommand(createCashBookRequestDto),
-      );
+  async execute(@Body() dto: CreateCashBookRequestDto): Promise<void> {
+    const result: Result<CashBookCreationError, void> =
+      await this.commandBus.execute(new CreateCashBookCommand(dto));
 
-    if (cashBook && cashBook.isFailure()) {
-      throw new BadRequestException(cashBook.value.getJoinedMessages());
+    // TODO Figure out why result may return undefined
+    if (result && result.isFailure()) {
+      throw new BadRequestException(result.value.getJoinedMessages());
     }
   }
 }

d301416023f93f85658173852d14208b503e2005
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-25 14:02:46 +0200

    Add COMMANDS and QUERIES constants in cash-books application layer

---
+++ src/cash-books/application/commands/index.ts
@@ -0,0 +1,4 @@
+import { AddReportingPeriodHandler } from './add-reporting-period/add-reporting-period.handle';
+import { CreateCashBookHandler } from './create-cash-book/create-cash-book.handle';
+
+export const COMMANDS = [AddReportingPeriodHandler, CreateCashBookHandler];
---
+++ src/cash-books/application/queries/index.ts
@@ -0,0 +1,3 @@
+import { GetCashBalanceHandler } from './get-cash-balance/get-cash-balance.handle';
+
+export const QUERIES = [GetCashBalanceHandler];

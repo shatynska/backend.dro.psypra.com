ab7c86f580c2b0dfb03f684579d84a4a2b981131
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-07 16:58:44 +0200

    Fix CashBookModule path in app.module

--- src/shared/infrastructure/app.module.ts
+++ src/shared/infrastructure/app.module.ts
@@ -3,9 +3,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { CashBooksModule } from '~/cash-books/infrastructure/cash-books.module';
 import { PrismaModule } from '~/shared/infrastructure/prisma/prisma.module';
 import { AuthModule } from '../../auth/auth.module';
-import { CashBooksModule } from '../../cash-books/infrastructure/cash-books.module';
 import { UsersModule } from '../../users/users.module';
 
 @Module({

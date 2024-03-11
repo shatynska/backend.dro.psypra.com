464eb2e5f07671307bcb4ee8449ae2481c7e418d
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-11 22:33:22 +0200

    Import PageSectionsModule to AppModule  & add page-sections alias to tsconfig

--- src/shared/infrastructure/app.module.ts
+++ src/shared/infrastructure/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { CashBooksModule } from '~/cash-books/infrastructure/cash-books.module';
+import { PageSectionsModule } from '~/page-sections/infrastructure/page-sections.module';
 import { PrismaModule } from '~/shared/infrastructure/prisma/prisma.module';
 import { AuthModule } from '../../auth/auth.module';
 import { UsersModule } from '../../users/users.module';
@@ -13,6 +14,7 @@ import { UsersModule } from '../../users/users.module';
     ConfigModule.forRoot({ isGlobal: true }),
     UsersModule,
     CashBooksModule,
+    PageSectionsModule,
     PrismaModule,
     AuthModule,
     ThrottlerModule.forRoot([

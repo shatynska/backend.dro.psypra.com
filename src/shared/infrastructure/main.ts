ae0da2b4542ec7e20af2c2ad659f165f6b16639d
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-11 22:44:02 +0200

    Separate swagger documents to book-keeping & website-content

--- src/shared/infrastructure/main.ts
+++ src/shared/infrastructure/main.ts
@@ -4,6 +4,10 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import helmet from 'helmet';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
+import { HttpModule } from '~/cash-books/infrastructure/http/http.module';
+import { PageSectionsHttpModule } from '~/page-sections/infrastructure/http/page-sections-http.module';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -14,15 +18,34 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
 
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('DroPsyPra')
-    .setDescription('The DroPsyPra API description')
+  const organizationBookkeepingOptions = new DocumentBuilder()
+    .setTitle('DroPsyPra book-keeping')
+    .setDescription('The DroPsyPra book-keeping API description')
     .setVersion('0.0.1')
-    .addTag('users')
+    .addTag('book-keeping')
     .addBearerAuth()
     .build();
-  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api/swagger', app, swaggerDocument);
+  const organizationBookkeepingDocument = SwaggerModule.createDocument(
+    app,
+    organizationBookkeepingOptions,
+    { include: [HttpModule, UsersModule, AuthModule] },
+  );
+  SwaggerModule.setup('api/book-keeping', app, organizationBookkeepingDocument);
+
+  const websiteContentOptions = new DocumentBuilder()
+    .setTitle('DroPsyPra website content')
+    .setDescription('The DroPsyPra website content API description')
+    .setVersion('0.0.1')
+    .addTag('pages')
+    .build();
+  const websiteContentDocument = SwaggerModule.createDocument(
+    app,
+    websiteContentOptions,
+    {
+      include: [PageSectionsHttpModule],
+    },
+  );
+  SwaggerModule.setup('api', app, websiteContentDocument);
 
   const config = await app.get(ConfigService);
   const port = config.get<number>('API_PORT');

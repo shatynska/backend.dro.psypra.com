9d25dbf4eecfd1ec1e5a7ff2771db1f9eeffdfd9
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-11 22:28:50 +0200

    Add GetHomeQuestionsPageSectionController & const PAGE_SECTIONS_CONTROLLERS

---
+++ src/website-content/page-sections/infrastructure/http/controllers/index.ts
@@ -0,0 +1,5 @@
+import { GetHomeQuestionsPageSectionController } from './get-home-questions-page-section/get-home-questions-page-section.controller';
+
+export const PAGE_SECTIONS_CONTROLLERS = [
+  GetHomeQuestionsPageSectionController,
+];

bb7dd300d9b27427b90f037228dc0cb6ad4e9450
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-13 17:10:12 +0200

    Refactor PageSectionHeaderMapper for consistency

--- src/website-content/page-sections/infrastructure/persistence/prisma/mappers/page-section-header.mapper.ts
+++ src/website-content/page-sections/infrastructure/persistence/prisma/mappers/page-section-header.mapper.ts
@@ -1,14 +1,14 @@
 import { PageSection } from '@prisma/client';
 import { PageSectionHeaderDto } from 'src/website-content/page-sections/application/dto/page-section-header.dto';
 
+type Props = Pick<PageSection, 'primaryHeading' | 'secondaryHeading' | 'href'>;
+
 export class PageSectionHeaderMapper {
-  static mapToDto(
-    params: Pick<PageSection, 'primaryHeading' | 'secondaryHeading' | 'href'>,
-  ): PageSectionHeaderDto {
+  static mapToDto(data: Props): PageSectionHeaderDto {
     const mappedData = {
-      primaryHeading: params.primaryHeading,
-      secondaryHeading: params.secondaryHeading,
-      href: params.href,
+      primaryHeading: data.primaryHeading,
+      secondaryHeading: data.secondaryHeading,
+      href: data.href,
     };
 
     return mappedData;

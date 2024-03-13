152a86bf8308846db24805b5eda4ebe1167d11ba
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-13 12:45:24 +0200

    Fix defaultMessage in PageSectionNotFoundError

--- src/website-content/page-sections/application/errors/page-section-not-found.error.ts
+++ src/website-content/page-sections/application/errors/page-section-not-found.error.ts
@@ -2,7 +2,8 @@ import { NotFoundError } from '~/shared/application/errors/not-found.error';
 
 export class PageSectionNotFoundError extends NotFoundError {
   // TODO Replace message with constant
-  static readonly defaultMessage = 'Секція не знайдена ';
+
+  static readonly defaultMessage = `Секцію не знайдено`;
 
   constructor(message: string = PageSectionNotFoundError.defaultMessage) {
     super(message);

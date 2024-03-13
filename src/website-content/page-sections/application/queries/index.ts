d315681c3bb6e92c03ccb5f5bef32be0db246894
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-13 12:51:21 +0200

    Fix typo in GetHomeQuestionsPageSectionHandler path name

--- src/website-content/page-sections/application/queries/index.ts
+++ src/website-content/page-sections/application/queries/index.ts
@@ -1,3 +1,3 @@
-import { GetHomeQuestionsPageSectionHandler } from './get-home-questions-page-section /get-home-questions-page-section.handle';
+import { GetHomeQuestionsPageSectionHandler } from './get-home-questions-page-section /get-home-questions-page-section.handler';
 
 export const PAGE_SECTIONS_QUERIES = [GetHomeQuestionsPageSectionHandler];

25bd1c3a6dcc774d0e9f4b47bc1bb78b13c6c798
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-14 23:39:55 +0200

    Fix typo in GetHomeQuestionsPageSectionQuery

--- src/website-content/page-sections/infrastructure/http/controllers/get-home-questions-page-section/get-home-questions-page-section.controller.ts
+++ src/website-content/page-sections/infrastructure/http/controllers/get-home-questions-page-section/get-home-questions-page-section.controller.ts
@@ -4,7 +4,7 @@ import { QueryBus } from '@nestjs/cqrs';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { HomeQuestionsPageSectionDto } from '~/page-sections/application/dto/home-questions-page-section.dto';
 import { PageSectionNotFoundError } from '~/page-sections/application/errors/page-section-not-found.error';
-import { GetHomeQuestionsPageSectionQuery } from '~/page-sections/application/queries/get-home-questions-page-section /get-home-questions-page-section.query';
+import { GetHomeQuestionsPageSectionQuery } from '~/page-sections/application/queries/get-home-questions-page-section/get-home-questions-page-section.query';
 import { Result } from '~/shared/core/result';
 import { HomeQuestionsPageSectionResponseDto } from '../../dto/responses/home-question-page-section/home-questions-page-section.response.dto';
 

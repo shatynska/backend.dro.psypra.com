4e22dc368bf2f791fb28ebdd78771d50c653898b
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-21 11:28:19 +0200

    Import Dimensions and Specialists modules to PageSectionsModule

--- src/website-content/page-sections/page-sections.module.ts
+++ src/website-content/page-sections/page-sections.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { DimensionsModule } from '../dimensions/dimensions.module';
+import { SpecialistsModule } from '../specialists/specialists.module';
 import { HttpModule } from './infrastructure/http/http.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, DimensionsModule, SpecialistsModule],
 })
 export class PageSectionsModule {}

60f84f41f5632692793de684bc51882e192e59de
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-25 23:50:50 +0200

    Add GetDimensionWithHref query

---
+++ src/website-content/dimensions/application/queries/get-dimension-with-href/get-dimension-with-href.query.ts
@@ -0,0 +1,5 @@
+import { GetDimensionWithHrefParametersDto } from '../../dto/dimension-with-href/get-dimension-with-href.parameters.dto';
+
+export class GetDimensionWithHrefQuery {
+  constructor(public readonly parameters: GetDimensionWithHrefParametersDto) {}
+}
--- src/website-content/dimensions/application/queries/index.ts
+++ src/website-content/dimensions/application/queries/index.ts
@@ -1,4 +1,9 @@
 import { GetDimensionItemHandler } from './get-dimension-item/get-dimension-item.handler';
+import { GetDimensionWithHrefHandler } from './get-dimension-with-href/get-dimension-with-href.handler';
 import { GetDimensionWithItemsHandler } from './get-dimension-with-items/get-dimension-with-items.handler';
 
-export const QUERIES = [GetDimensionWithItemsHandler, GetDimensionItemHandler];
+export const QUERIES = [
+  GetDimensionItemHandler,
+  GetDimensionWithHrefHandler,
+  GetDimensionWithItemsHandler,
+];

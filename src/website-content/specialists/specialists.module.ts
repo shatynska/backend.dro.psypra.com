95823e99293dbf765d9a90cb98938eb5ef8206ab
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-26 18:13:24 +0200

    Import CqrsModule & DimensionsModule to SpecialistsModule

--- src/website-content/specialists/specialists.module.ts
+++ src/website-content/specialists/specialists.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { DimensionsModule } from '~/dimensions/dimensions.module';
 import { QUERIES } from './application/queries';
 import { PersistenceModule } from './infrastructure/persistence/persistence.module';
 
 @Module({
-  imports: [PersistenceModule],
+  imports: [CqrsModule, PersistenceModule, DimensionsModule],
   providers: [...QUERIES],
   exports: [...QUERIES],
 })

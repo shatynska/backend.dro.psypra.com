b8a6779c5086f8c3ea9a76fc84bfec3cca6405cb
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-27 12:35:27 +0200

    Add GetSpecialistAdditional controller with dto in PageSections module

---
+++ src/website-content/page-sections/infrastructure/http/controllers/get-specialist-additional/get-specialist-additional.controller.ts
@@ -0,0 +1,41 @@
+import { ApiErrorDecorator, Public } from '@common/decorators';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  NotFoundException,
+  Param,
+} from '@nestjs/common';
+import { QueryBus } from '@nestjs/cqrs';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { SectionNotFoundError } from '~/page-sections/application/errors/section-not-found.error';
+import { GetSpecialistAdditionalQuery } from '~/page-sections/application/queries/get-specialist-additional/get-specialist-additional.query';
+import { SpecialistAdditionalResponseDto } from '../../dto/specialist-additional/specialist-additional.response.dto';
+
+@Controller('pages')
+@Public()
+@ApiTags('pages')
+export class GetSpecialistAdditionalController {
+  constructor(private readonly queryBus: QueryBus) {}
+
+  @ApiResponse({
+    status: 200,
+    type: SpecialistAdditionalResponseDto,
+  })
+  @ApiErrorDecorator(HttpStatus.NOT_FOUND, SectionNotFoundError.defaultMessage)
+  @Get('specialists/:specialist/:section')
+  async execute(
+    @Param('specialist') specialist: string,
+    @Param('section') section: string,
+  ): Promise<SpecialistAdditionalResponseDto> {
+    const data = await this.queryBus.execute(
+      new GetSpecialistAdditionalQuery(specialist, section),
+    );
+
+    if (data.isFailure()) {
+      throw new NotFoundException(data.value.message);
+    }
+
+    return new SpecialistAdditionalResponseDto(data.value);
+  }
+}

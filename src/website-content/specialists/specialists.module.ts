790aea31d5287ae8163ade53aff2c01bb714d9e7
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-29 10:42:06 +0200

    Remove persistence.module file & put repository provider directly to specialists module

--- src/website-content/specialists/specialists.module.ts
+++ src/website-content/specialists/specialists.module.ts
@@ -2,11 +2,18 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { DimensionsModule } from '~/dimensions/dimensions.module';
 import { QUERIES } from './application/queries';
-import { PersistenceModule } from './infrastructure/persistence/persistence.module';
+import { READ_REPOSITORY_TOKEN } from './application/read.repository';
+import { PrismaReadRepository } from './infrastructure/persistence/prisma/read.repository';
 
 @Module({
-  imports: [CqrsModule, PersistenceModule, DimensionsModule],
-  providers: [...QUERIES],
+  imports: [CqrsModule, DimensionsModule],
+  providers: [
+    ...QUERIES,
+    {
+      provide: READ_REPOSITORY_TOKEN,
+      useClass: PrismaReadRepository,
+    },
+  ],
   exports: [...QUERIES],
 })
 export class SpecialistsModule {}

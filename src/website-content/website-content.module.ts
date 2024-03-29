17a4513c0a7f3050e2e21ad9efc3bc48ffe104a5
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-29 23:46:05 +0200

    Import Cqrs, Specialists, Dimensions modules & define controllers in WebsiteContentModule

--- src/website-content/website-content.module.ts
+++ src/website-content/website-content.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { DimensionsModule } from './dimensions/dimensions.module';
+import { CONTROLLERS as PAGE_SECTIONS_CONTROLLERS } from './page-sections/infrastructure/http/controllers';
 import { PageSectionsModule } from './page-sections/page-sections.module';
+import { SpecialistsModule } from './specialists/specialists.module';
 
 @Module({
-  imports: [PageSectionsModule],
+  imports: [
+    CqrsModule,
+    PageSectionsModule,
+    SpecialistsModule,
+    DimensionsModule,
+  ],
+  controllers: [...PAGE_SECTIONS_CONTROLLERS],
 })
 export class WebsiteContentModule {}

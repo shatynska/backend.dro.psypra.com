8d5489620886da39a563c8843e4d36573bd71466
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-30 23:55:12 +0200

    Remove GetSpecialistAdditional & GetSpecialistMain controllers in PageSections module

--- src/website-content/page-sections/infrastructure/http/controllers/index.ts
+++ src/website-content/page-sections/infrastructure/http/controllers/index.ts
@@ -1,13 +1,9 @@
 import { GetDimensionMainController } from './get-dimension-main/get-dimension-main.controller';
 import { GetHomeDimensionController } from './get-home-dimension/get-home-dimension.controller';
 import { GetHomeQuestionsController } from './get-home-questions/get-home-questions.controller';
-import { GetSpecialistAdditionalController } from './get-specialist-additional/get-specialist-additional.controller';
-import { GetSpecialistMainController } from './get-specialist-main/get-specialist-main.controller';
 
 export const CONTROLLERS = [
   GetHomeQuestionsController,
   GetHomeDimensionController,
   GetDimensionMainController,
-  GetSpecialistMainController,
-  GetSpecialistAdditionalController,
 ];

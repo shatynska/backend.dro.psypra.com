02f5f5b0d148c695c84f005323023aa8be8c6e44
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-31 15:28:26 +0300

    Add specialists & dimensions controllers to WebsiteContent module

--- src/website-content/website-content.module.ts
+++ src/website-content/website-content.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { DimensionsModule } from './dimensions/dimensions.module';
+import { CONTROLLERS as DIMENSIONS_CONTROLLERS } from './dimensions/infrastructure/http/controllers';
 import { CONTROLLERS as PAGE_SECTIONS_CONTROLLERS } from './page-sections/infrastructure/http/controllers';
 import { PageSectionsModule } from './page-sections/page-sections.module';
+import { CONTROLLERS as SPECIALISTS_CONTROLLERS } from './specialists/infrastructure/http/controllers';
 import { SpecialistsModule } from './specialists/specialists.module';
 
 @Module({
@@ -12,6 +14,10 @@ import { SpecialistsModule } from './specialists/specialists.module';
     SpecialistsModule,
     DimensionsModule,
   ],
-  controllers: [...PAGE_SECTIONS_CONTROLLERS],
+  controllers: [
+    ...PAGE_SECTIONS_CONTROLLERS,
+    ...DIMENSIONS_CONTROLLERS,
+    ...SPECIALISTS_CONTROLLERS,
+  ],
 })
 export class WebsiteContentModule {}

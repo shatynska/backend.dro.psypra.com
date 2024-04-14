41e953896a1dec981919d39b1c2ea05efbd663f7
Helen Shatynska <helen.shatynska@gmail.com>
2024-04-15 00:48:56 +0300

    Fix zod schema in LoginDto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,15 +1,9 @@
 import { createZodDto } from 'nestjs-zod';
 import { z } from 'zod';
 
-export const LoginDtoSchema = z
-  .object({
-    identifier: z.string().min(2).max(40),
-    password: z.string().min(8).max(40),
-    passportRepeat: z.string().min(8).max(40),
-  })
-  .refine((data) => data.password === data.passportRepeat, {
-    path: ['passportRepeat'],
-    message: 'Паролі не співпадають',
-  });
+export const LoginDtoSchema = z.object({
+  identifier: z.string().min(2).max(40),
+  password: z.string().min(8).max(40),
+});
 
 export class LoginDto extends createZodDto(LoginDtoSchema) {}
